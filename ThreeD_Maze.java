6924bb18810d9042fa8b2f724d3e54445c739d1c
Chen Ling <evan1282467574@gmail.com>
2020-11-18 17:14:05 +0800

    Update and rename homework.java to ThreeD_Maze.java

--- ThreeD_Maze.java
+++ ThreeD_Maze.java
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class homework {
+public class ThreeD_Maze {
 
 	public static void main(String[] args) {
 		// Setup
